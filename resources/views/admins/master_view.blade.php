d8a4363f89a6eb0cb2a420ecb41ff797e14cce56
timon162 <duyle8889@gmail.com>
2026-01-22 15:58:17 +0700

    update footer

--- resources/views/admins/master_view.blade.php
+++ resources/views/admins/master_view.blade.php
@@ -11,23 +11,29 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <title>Document</title>
 
-    <link rel="stylesheet" href="{{ asset('css/admins/partials/header-admin-view.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admins/partials/header_admin_view.css') }}">
     <link rel="stylesheet"
         href="{{ asset('css/admins/partials/detail_title_headers/detail_title_women_fashion.css') }}">
     <link rel="stylesheet" href="{{ asset('css/admins/partials/detail_title_headers/detail_title_men_fashion.css') }}">
     <link rel="stylesheet"
         href="{{ asset('css/admins/partials/detail_title_headers/detail_title_album_fashion.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admins/partials/footer_admin_view.css') }}">
     <link rel="stylesheet" href="{{ asset('css/admins/contents/home_content.css') }}">
     <link rel="stylesheet" href="{{ asset('css/admins/contents/hot_product.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/admins/admin-master-view.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admins/admin_master_view.css') }}">
 </head>
 
 <body>
     <header class="header-admin-view d-flex align-items-center justify-content-center px-3">
         @include('admins.partials.header')
     </header>
-    <div class="content-admin-view">
+    <div class="content-admin-view pb-3">
         @yield('main-content-admin-view')
+    </div>
+    <div class="line-center-content-footer d-flex w-100 justify-content-center align-items-center p-4 gap-3">
+        <span></span>
+        <h3>TIMON STORE</h3>
+        <span></span>
     </div>
     <footer class="footer-admin-view">
         @include('admins.partials.footer')
